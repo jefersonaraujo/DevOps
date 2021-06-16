8ae705e69ac9250ca9afdd56139888ae1d1a5649
jeferson <jefersonaraujo95@gmail.com>
2021-06-15 22:17:50 -0300

    output and key

--- Terraform/DigitalOcean/main.tf
+++ Terraform/DigitalOcean/main.tf
@@ -1,4 +1,8 @@
-provider "digitalocean" {}
+provider "digitalocean" {
+    token = var.do_token
+}
+
+variable "do_token" {}
 
 terraform {
     required_providers {
@@ -12,4 +16,10 @@ resource "digitalocean_droplet" "web" {
     image   = "ubuntu-20-04-x64"
     name    = "web-1"
     region  = "nyc1"
-    size    = "s-1vcpu-1gb
+    size    = "s-1vcpu-1gb"
+    ssh_keys = ["27:51:6b:74:32:ff:2e:8a:d4:d7:ba:c7:f5:d3:1f:f9"]
+}
+
+output "ip_do_droplet" {
+    value = digitalocean_droplet.web.ipv4_address
+}
