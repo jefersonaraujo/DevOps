f94e9bda7a268d7241f5a5f220435549b995523e
jeferson <jefersonaraujo95@gmail.com>
2021-06-19 16:57:37 -0300

    organlizando file db

---
+++ Terraform/DigitalOcean/loadbalancer.tf
@@ -0,0 +1,19 @@
+resource "digitalocean_loadbalancer" "public" {
+  name   = "loadbalancer-1"
+  region = var.droplet_region
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = digitalocean_droplet.web[*].id
+}
