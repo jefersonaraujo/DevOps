378ba2361bc0c22ebc319e1c6b2018b21a60ad7c
jeferson <jefersonaraujo95@gmail.com>
2021-06-21 19:37:14 -0300

    update output

--- Terraform/DigitalOcean/output.tf
+++ Terraform/DigitalOcean/output.tf
@@ -7,12 +7,12 @@ output "load_balancer_ip" {
   value = digitalocean_loadbalancer.public.ip
 }
 
-# output "database_password" {
-#   value = digitalocean_database_user.username.password
-#   sensitive = true
-# }
+output "database_password" {
+  value = digitalocean_database_user.username.password
+  sensitive = true
+}
 
-# output "database_uri" {
-#   value = digitalocean_database_cluster.postgres.uri
-# }
+output "database_uri" {
+  value = nonsensitive(digitalocean_database_cluster.postgres.uri)
+}
 
